cc8c454d2d865146edf663aef4a046d7576d11b7
Pinal Bhadani <pd3257@hotmail.com>
2018-05-26 16:54:07 +0530

    Added project

---
+++ IVC.Return/Response.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace IVC.Return
+{
+    public class Response<T>
+    {
+        public int StatusCode { get; set; }
+        public string Message { get; set; }
+        public Exception Exception { get; set; }
+        public string ExceptionStackTrace { get; set; }
+        public T Content { get; set; }
+    }
+
+    public class Response
+    {
+        public int StatusCode { get; set; }
+        public string Message { get; set; }
+        public Exception Exception { get; set; }
+        public string ExceptionStackTrace { get; set; }
+    }
+
+}
---
+++ IVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace IVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
